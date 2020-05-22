8f6fd589fcacf39704b79588cdbd42439b4f9456
Mar-babu <ar_cse@yahoo.com>
2020-05-23 00:38:13 +0600

    login form, icon, field, background

--- src/App.svelte
+++ src/App.svelte
@@ -1,30 +1,26 @@
 <script>
-	export let name;
+  import Login from "./Login.svelte";
 </script>
 
-<main>
-	<h1>Hello {name}!</h1>
-	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
-</main>
-
 <style>
-	main {
-		text-align: center;
-		padding: 1em;
-		max-width: 240px;
-		margin: 0 auto;
-	}
+  main {
+    display: flex;
+    align-items: center;
+	flex-direction: column;
+	font-family: sofia-pro, sans-serif;
+	text-align: center;
+  }
 
-	h1 {
-		color: #ff3e00;
-		text-transform: uppercase;
-		font-size: 4em;
-		font-weight: 100;
-	}
-
-	@media (min-width: 640px) {
-		main {
-			max-width: none;
-		}
-	}
+  h1 {
+    font-family: gothic-open-shaded, sans-serif;
+    font-weight: 400;
+    font-style: normal;
+  }
 </style>
+
+<main>
+
+  <h1>Sign in</h1>
+
+  <Login />
+</main>
